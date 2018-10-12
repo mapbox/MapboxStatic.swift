96855921c64558f43383e07166e530f158ec02e7
Harish Yerra <hyerra08@gmail.com>
2018-10-12 16:49:16 -0500

    UIApplication.LaunchOptionsKey isn't backwards compatible so make sure to check swift version before using it.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -1,12 +1,18 @@
 import UIKit
 
+#if swift(>=4.2)
+typealias LaunchOptionsKey = UIApplication.LaunchOptionsKey
+#else
+typealias LaunchOptionsKey = UIApplicationLaunchOptionsKey
+#endif
+
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window!.rootViewController = ViewController()
         window!.makeKeyAndVisible()
